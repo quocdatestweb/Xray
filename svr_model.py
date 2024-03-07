ef44327087b53b5337470aa8b639fac0b0363de6
quocdatestweb <147794210+quocdatestweb@users.noreply.github.com>
2024-03-07 21:06:14 +0700

    Update svr_model.py

--- svr_model.py
+++ svr_model.py
@@ -53,5 +53,8 @@ def home_page():
         return render_template('index.html')
 
 
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=True)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run()
